46d18ef6748cad4c4d5ea167b38f9acd9b35628f
ANAND <ClobberXD@gmail.com>
2019-05-04 14:09:34 +0530

    Initial commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+callbacks_api = {}
+function callbacks_api.register_callback(cb_registrar, cb, params)
+	if not cb_registrar then
+		minetest.log("warning", "callbacks_api: Callback registrar does not exist.")
+		return
+	end
+
+	assert(cb_registrar and type(cb_registrar) == "function",
+		"callbacks_api: Callback registrar passed to register_callback is not a function!")
+
+	cb_registrar(cb, params)
+end
