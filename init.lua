3853db32c3d350ff43bcf7ac92deba0068477794
ANAND <ClobberXD@gmail.com>
2019-05-04 15:35:36 +0530

    Actually allow passing opt. params

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 callbacks_api = {}
-function callbacks_api.register_callback(cb_registrar, cb, params)
+function callbacks_api.register_callback(cb_registrar, cb, ...)
 	if not cb_registrar then
 		minetest.log("warning", "callbacks_api: Callback registrar does not exist.")
 		return
@@ -8,5 +8,5 @@ function callbacks_api.register_callback(cb_registrar, cb, params)
 	assert(cb_registrar and type(cb_registrar) == "function",
 		"callbacks_api: Callback registrar passed to register_callback is not a function!")
 
-	cb_registrar(cb, params)
+	cb_registrar(cb, ...)
 end
